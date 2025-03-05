4868ae55fff38e58ad5f301b177c75a89e6e8c18
LeiamNash <94122613+LeiamNashRebirth@users.noreply.github.com>
2025-03-05 15:21:43 +0000

    navigation removed on login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,11 @@ import "./globals.css";
 import AuthGuard from "@/components/AuthGuard";
 import Navigation from "@/components/Navigation";
 import NextTopLoader from "nextjs-toploader";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <body className="bg-gray-900 text-white">
@@ -11,7 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
         <AuthGuard>
           {children}
-          <Navigation />
+        {pathname !== "/login" && <Navigation />}
         </AuthGuard>
       </body>
     </html>
